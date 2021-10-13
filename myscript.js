7a6e57862b1a3a7634bf088b9abd73bc48aa7674
志賀大河 <gaitatg97@gmail.com>
2021-10-13 19:57:34 +0200

    makeing seat

---
+++ myscript.js
@@ -0,0 +1,34 @@
+
+let makeRows = (sectionLength, rowLength, placement) => {
+  const rows = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t"];
+
+  let html = "";
+  let counter = 1;
+
+  rows.forEach((row) => {
+
+    switch (placement) {
+      case "left": html += `<div class="label">${row}</div>`; break;
+      case "right": counter = counter + (rowLength - sectionLength); break;
+      default: counter = counter + ((rowLength - sectionLength) / 2); break;
+    }
+
+    for (let i = 0; i < sectionLength; i++) { 
+      html += `<div class="a" id=${row + counter}>${counter}</div>`
+      counter++;
+    }
+
+    switch (placement) {
+      case "left": counter = counter + (rowLength - sectionLength); break;
+      case "right": html += `<div class="label">${row}</div>`; break;
+      default: counter = counter + ((rowLength - sectionLength) / 2); break;
+    }
+
+  });
+
+  document.getElementById(placement).innerHTML = html;
+}
+
+makeRows(3,15, "left");
+makeRows(3, 15, "right");
+makeRows(9, 15, "middle");
