95628ee02e51a61e4eb09017331df2b24ced3311
志賀大河 <gaitatg97@gmail.com>
2021-10-13 21:09:02 +0200

    add form

--- myscript.js
+++ myscript.js
@@ -1,3 +1,34 @@
+let reservedSeats = {
+	record1: {
+		seat: "b19",
+		owner: {
+			fname: "Joe",
+			lname: "Smith"
+		}
+	},
+	record2: {
+		seat: "b20",
+		owner: {
+			fname: "Joe",
+			lname: "Smith"
+		}
+	},
+	record3: {
+		seat: "b21",
+		owner: {
+			fname: "Joe",
+			lname: "Smith"
+		}
+	},
+	record4: {
+		seat: "b22",
+		owner: {
+			fname: "Joe",
+			lname: "Smith"
+		}
+	}
+};
+
 
 let makeRows = (sectionLength, rowLength, placement) => {
   const rows = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t"];
@@ -5,7 +36,7 @@ let makeRows = (sectionLength, rowLength, placement) => {
   let html = "";
   let counter = 1;
 
-  rows.forEach((row) => {
+  rows.forEach(row => {
 
     switch (placement) {
       case "left": html += `<div class="label">${row}</div>`; break;
@@ -32,3 +63,37 @@ let makeRows = (sectionLength, rowLength, placement) => {
 makeRows(3,15, "left");
 makeRows(3, 15, "right");
 makeRows(9, 15, "middle");
+
+// making reserved seats
+for (const key in reservedSeats) { 
+  const obj = reservedSeats[key];
+  
+  document.getElementById(obj.seat).className = "r";
+  document.getElementById(obj.seat).innerHTML = "R";
+}
+  
+(function () { 
+  "use strict";
+
+  let selectSeats = [];
+  let seats = document.querySelectorAll(".a");
+
+  seats.forEach(seat => {
+    seat.addEventListener("click", (event) => {
+      seatSelectProcess(seat.id);
+    });  
+  });
+
+  let seatSelectProcess = (thisSeatId) => { 
+    let index = selectSeats.indexOf(thisSeatId);
+    if (index > -1) {
+      selectSeats.splice(index, 1);
+      document.getElementById(thisSeatId).className = "a";
+    } else { 
+      selectSeats.push(thisSeatId);
+      document.getElementById(thisSeatId).className = "s";
+    }
+    console.log(selectSeats);
+  }
+
+})();
